eab5027a05e1ed198f227707022c8cc97b45562e
usleech <71797515+usleech@users.noreply.github.com>
2021-04-14 14:34:25 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+# DHT
+wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
+
+TRACKER=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',')
+ran=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+
+echo "Update tracker $TRACKER"
+echo "bt-tracker=$TRACKER" >> /app/aria2.conf
+
+echo "Update peer-id-prefix=-TR2940-$ran"
+echo "peer-id-prefix=-TR2940-$ran" >> /app/aria2.conf
+
+mkdir /app/gautam
+wget -O /app/gautam/gclone.gz https://git.io/JJMSG
+gzip -d /app/gautam/gclone.gz
+
+chmod +x /app/extract /app/gautam/gclone
